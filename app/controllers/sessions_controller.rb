7abe3b4a78080b500d80d80282672558a8f95cb8
sespinosa0128 <sespinosa@college.harvard.edu>
2014-03-13 16:13:22 -0400

    Add user auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+ def new
+ end
+
+ def create
+  user = User.find_by(email: params[:session][:email])
+  if user
+    if user.authenticate(params[:session][:password])
+      cookies.signed[:user_id] = user.id
+    end
+  end
+  redirect_to root_path
+end
+
+def destroy
+  cookies.delete :user_id 
+  redirect_to root_path
+end
+
+end
